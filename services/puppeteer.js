132356976bcad1a242760e2a7a91aa63dd2c01e4
iSoumya <isoumyamahata@gmail.com>
2020-09-20 22:53:09 +0530

    Puppeter args change
    Signed-off-by: iSoumya <isoumyamahata@gmail.com>

--- services/puppeteer.js
+++ services/puppeteer.js
@@ -5,19 +5,10 @@ class PuppeteerService {
 
   async init() {
     this.browser = await puppeteer.launch({
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-infobars',
-        '--window-position=0,0',
-        '--ignore-certifcate-errors',
-        '--ignore-certifcate-errors-spki-list',
-        '--incognito',
-        '--proxy-server=http=194.67.37.90:3128',
-        // '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"', //
-      ],
-      // headless: false,
-    });
+                                              headless: false,
+                                              slowMo: 80,
+                                              args: ['--window-size=1920,1080']
+                                              });
   }
 
   /**
